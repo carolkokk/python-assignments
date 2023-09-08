54699c906202779d681fbc10abd687bd4c8ec243
carolkokk <142995715+carolkokk@users.noreply.github.com>
2023-09-08 22:09:54 +0300

    Add files via upload

---
+++ Exercise 5+6(Module 3).py
@@ -0,0 +1,126 @@
+# Exercise 5.1
+import random
+times = int(input("How many dices to roll?"))
+diceNumbers = []
+sum = 0
+
+for i in range(0,times):
+    randomNumber = random.randint(1,6)
+    diceNumbers.append(randomNumber)
+    sum = sum + randomNumber
+
+print(sum)
+
+#Exercise 5.2
+number = input("Enter the first number, put empty string to quit.")
+numbers = []
+
+while (number != ""):
+    numbers.append(int(number))
+    number = input("Enter the next number, put empty string to quit.")
+
+numbers.sort(reverse=True)
+
+for i in range(0,5):
+    print(numbers[i])
+
+# Exercise 5.3
+number = int(input("Type in a positive integer: \n"))
+primeInteger = True
+
+for i in range(2,number):
+    if(number % i == 0):
+        primeInteger = False
+
+if(primeInteger == True):
+    print("The number is a prime number.")
+else:
+    print("The number is not a prime number.")
+
+#Exercise 5.4
+cityNames = []
+name = input("Enter the names of five cities one by one:")
+cityNames.append(name)
+
+for n in range(0,4):
+    name = input()
+    cityNames.append(name)
+    n= n+1
+
+for i in range(0,5):
+    print(cityNames[i])
+
+#Exercise 6.1
+import random
+diceNumber1 = 0
+def randomDiceRoll1():
+    randomResult1 = random.randint(1,6)
+    return randomResult1
+
+while (diceNumber1!= 6):
+    diceNumber1 = randomDiceRoll1()
+    print(diceNumber1)
+
+#Exercise 6.2
+import random
+maximumDiceSides = int(input("what is the number of sides on the dice do you want to have? "))
+diceNumber2 = 0
+def randomDiceRoll2(diceSides):
+    randomResult2 = random.randint(1,diceSides)
+    return randomResult2
+
+while (diceNumber2!= maximumDiceSides):
+    diceNumber2 = randomDiceRoll2(maximumDiceSides)
+    print(diceNumber2)
+
+#Exercise 6.3
+inputGallon = float(input("What is the volume of American gallon that you want to convert? Stop by putting a negative value: \n"))
+def usGallonToLiter (input):
+    result = input * 3.785
+    return result
+
+while(inputGallon >= 0):
+    print(f"{inputGallon} American gallon(s) are {usGallonToLiter(inputGallon):.2f} liter(s).")
+    inputGallon = float(input("What is the volume of American gallon that you want to convert? Stop by putting a negative value: \n"))
+
+#Exercise 6.4
+def sumOfNumbers1(integerNumbers):
+    sum1 = 0
+    for integerNumber1 in integerNumbers:
+        sum1 = sum1 + integerNumber1
+    return sum1
+
+list1 = [6,5,4,8,9,7,3]
+print(sumOfNumbers1(list1))
+
+#Exercise 6.5
+def newListCreated (integerNumbers2):
+    newList = []
+    for integerNumber2 in integerNumbers2:
+        if(integerNumber2 % 2 == 0 and integerNumber2 != 0):
+            newList.append(integerNumber2)
+    return newList
+
+list2 = [0,6,5,4,8,9,7,3]
+print(list2)
+print(newListCreated(list2))
+
+#Exercise 6.6
+import math
+diameterOfPizza1 = float(input("Insert the diameter of the first pizza: \n"))
+priceOfPizza1 = float(input("Insert the price of the first pizza: \n"))
+diameterOfPizza2 = float(input("Insert the diameter of the second pizza: \n"))
+priceOfPizza2 = float(input("Insert the price of the second pizza: \n"))
+
+def avePrice (diameterOfPizza,priceOfPizza):
+    result = priceOfPizza/ math.pi * (diameterOfPizza/2)**2
+    return result
+pizza1Ave = avePrice(diameterOfPizza1,priceOfPizza1)
+pizza2Ave = avePrice(diameterOfPizza2,priceOfPizza2)
+
+if(pizza1Ave > pizza2Ave):
+    print("The first pizza provides better value for money.")
+if(pizza2Ave > pizza1Ave):
+    print("The second pizza provides better value for money.")
+else:
+    print("They have the same value for money.")
